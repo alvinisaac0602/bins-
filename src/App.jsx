249ab35a7a5c8a29871e1c9135351fe0a7855f60
Sseku Jovan <ssekujovan316@gmail.com>
2026-03-24 20:11:39 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Payments from "./pages/Payments";
 import News from "./pages/News";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
@@ -22,6 +22,7 @@ export default function App() {
         <Route path="/news" element={<News />} />
       </Routes>
       <Footer />
+      <Analytics />
     </BrowserRouter>
   );
 }
